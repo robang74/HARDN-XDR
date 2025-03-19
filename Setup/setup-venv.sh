e40f5363e7186681b11aefa1eb47415cdd661142
LinuxUser255 <atnl@protonmailcom>
2025-03-19 01:46:16 -0400

    script

--- Setup/setup-venv.sh
+++ Setup/setup-venv.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 # Create Python virtual environment and install dependencies
-setup_venv() {
-    printf "\e[1;31m[+] Setting up Python virtual environment...\e[0m\n"
-    python3 -m venv venv
-    source venv/bin/activate
-    if [[ -f requirements.txt ]]; then
-        pip install -r requirements.txt
-    else
-        printf "\e[1;31mRequirements.txt not found. Skipping Python dependencies installation.\e[0m\n"
-    fi
-}
-setup_venv
+printf "\e[1;31m[+] Setting up Python virtual environment...\e[0m\n"
+python3 -m venv venv
+
+# sleep for 3 seconds before activating
+time.sleep 3
+source venv/bin/activate
