416556ac19518939dc3d35fcfe85a38f8218cb26
Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2025-05-08 22:20:18 -0400

    Update hardn-setup.sh

--- src/setup/hardn-setup.sh
+++ src/setup/hardn-setup.sh
@@ -267,44 +267,42 @@ EOF
 }
 
 stig_kernel_setup() {
-    printf "\033[1;31m[+] Setting up STIG-compliant kernel parameters...\033[0m\n"
-    tee /etc/sysctl.d/stig-kernel.conf > /dev/null <<EOF
-kernel.randomize_va_space = 2        
-kernel.exec-shield = 1               
-kernel.panic_on_oops = 1             
-kernel.panic = 10                    
-kernel.kptr_restrict = 2             
-kernel.dmesg_restrict = 1           
-fs.protected_hardlinks = 1           
-fs.protected_symlinks = 1            
-net.ipv4.conf.all.rp_filter = 1     
+    printf "\033[1;31m[+] Setting up STIG-compliant kernel parameters (login-safe)...\033[0m\n"
+    tee /etc/sysctl.d/stig-kernel-safe.conf > /dev/null <<EOF
+kernel.randomize_va_space = 2
+kernel.kptr_restrict = 2
+kernel.dmesg_restrict = 1
+fs.protected_hardlinks = 1
+fs.protected_symlinks = 1
+
+net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
-net.ipv4.conf.all.accept_redirects = 0  
+net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.default.accept_redirects = 0
-net.ipv4.conf.all.secure_redirects = 0  
+net.ipv4.conf.all.secure_redirects = 0
 net.ipv4.conf.default.secure_redirects = 0
-net.ipv4.conf.all.send_redirects = 0    
+net.ipv4.conf.all.send_redirects = 0
 net.ipv4.conf.default.send_redirects = 0
-net.ipv4.tcp_syncookies = 1            
-net.ipv4.conf.all.log_martians = 1     
+net.ipv4.tcp_syncookies = 1
+net.ipv4.conf.all.log_martians = 1
 net.ipv4.conf.default.log_martians = 1
-net.ipv4.icmp_echo_ignore_broadcasts = 1  
-net.ipv4.icmp_ignore_bogus_error_responses = 1  
-net.ipv4.tcp_rfc1337 = 1                 
-net.ipv4.conf.all.accept_source_route = 0  
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.tcp_rfc1337 = 1
+net.ipv4.conf.all.accept_source_route = 0
 net.ipv4.conf.default.accept_source_route = 0
-net.ipv4.conf.all.forwarding = 0         
-net.ipv4.conf.default.forwarding = 0
 EOF
 
     sysctl --system || printf "\033[1;31m[-] Failed to reload sysctl settings.\033[0m\n"
     sysctl -w kernel.randomize_va_space=2 || printf "\033[1;31m[-] Failed to set kernel.randomize_va_space.\033[0m\n"
 }
 
+
 stig_disable_usb() {
     echo "install usb-storage /bin/false" > /etc/modprobe.d/hardn-blacklist.conf
     update-initramfs -u || printf "\033[1;31m[-] Failed to update initramfs.\033[0m\n"
 }
+
 
 stig_disable_core_dumps() {
     echo "* hard core 0" | tee -a /etc/security/limits.conf > /dev/null
