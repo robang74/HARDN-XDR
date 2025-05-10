7500703a271d85436e6445c8622a57f2e750f301
opensource-for-freedom <timothydaniel14@yahoo.com>
2025-05-10 12:29:55 -0400

    fixed rkhunter perm's and set mirror dir

--- src/setup/hardn-setup.sh
+++ src/setup/hardn-setup.sh
@@ -164,18 +164,21 @@ enable_aide() {
 enable_rkhunter(){
     printf "\033[1;31m[+] Installing rkhunter...\033[0m\n"
     if ! apt install -y rkhunter; then
-        printf "\033[1;33m[-] rkhunter install failed, skipping rkhunter setup.\033[0m\n"
         return 0
     fi
 
-    sed -i 's|^WEB_CMD=.*|#WEB_CMD=|' /etc/rkhunter.conf
-    sed -i 's|^MIRRORS_MODE=.*|MIRRORS_MODE=1|' /etc/rkhunter.conf
-    chown -R root:root /var/lib/rkhunter
-    chmod -R 755 /var/lib/rkhunter
+    # Fix permissions for rkhunter data directory
+    sudo chown -R root:root /var/lib/rkhunter
+    sudo chmod -R 755 /var/lib/rkhunter
 
+    # Ensure mirrors and update settings are correct
+    sed -i 's|^#*MIRRORS_MODE=.*|MIRRORS_MODE=1|' /etc/rkhunter.conf
+    sed -i 's|^#*UPDATE_MIRRORS=.*|UPDATE_MIRRORS=1|' /etc/rkhunter.conf
+    sed -i 's|^WEB_CMD=.*|#WEB_CMD=|' /etc/rkhunter.conf
+
+    # Try to update rkhunter data files
     if ! rkhunter --update; then
-        printf "\033[1;33m[-] rkhunter update failed, skipping propupd.\033[0m\n"
-        return 0
+        printf "\033[1;33m[!] rkhunter update failed. Check your network connection or proxy settings.\033[0m\n"
     fi
 
     rkhunter --propupd
@@ -434,6 +437,7 @@ setup_complete() {
     else
         printf "\033[1;31m[-] hardn-packages.sh not found at: %s. Skipping...\033[0m\n" "$PACKAGES_SCRIPT"
     fi
+
 }
 
 main() {
