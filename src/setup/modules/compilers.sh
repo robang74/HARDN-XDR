243ad91291caf647c76cb4788047b9ce57f1e1c1
Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2025-07-01 03:12:49 -0400

    Securejump (#106)
    add changelog
    
    ---------
    
    Co-authored-by: Chris <atnl@protonmail.com>
    Co-authored-by: 4nt1 <64175868+4nt11@users.noreply.github.com>
    Co-authored-by: opensource-for-freedom <timothydaniel14@yaho.com>

--- src/setup/modules/compilers.sh
+++ src/setup/modules/compilers.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-main() {
-    HARDN_STATUS "error" "Restricting compiler access to root only (HRDN-7222)..."
+HARDN_STATUS "error" "Restricting compiler access to root only (HRDN-7222)..."
 
-    local compilers
-    compilers="/usr/bin/gcc /usr/bin/g++ /usr/bin/make /usr/bin/cc /usr/bin/c++ /usr/bin/as /usr/bin/ld"
-    for bin in $compilers; do
-        if [[ -f "$bin" ]]; then
-            chmod 755 "$bin"
-            chown root:root "$bin"
-            HARDN_STATUS "pass" "Set $bin to 755 root:root (default for compilers)."
-        fi
-    done
-}
+local compilers
+compilers="/usr/bin/gcc /usr/bin/g++ /usr/bin/make /usr/bin/cc /usr/bin/c++ /usr/bin/as /usr/bin/ld"
+for bin in $compilers; do
+	if [[ -f "$bin" ]]; then
+		chmod 755 "$bin"
+		chown root:root "$bin"
+		HARDN_STATUS "pass" "Set $bin to 755 root:root (default for compilers)."
+	fi
+done
 
-main
