0b1117d7a0953f6e0db082a860923beaaffd0058
Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>
2025-08-18 06:04:41 -0400

    Update MatrixBackground.tsx
    Signed-off-by: Tim Burns <92951498+OpenSource-For-Freedom@users.noreply.github.com>

--- src/components/MatrixBackground.tsx
+++ src/components/MatrixBackground.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
-
+// the mox background 
 export default function MatrixBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
